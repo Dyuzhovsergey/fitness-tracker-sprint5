e0591130a63d6ad25a6d68d7b594f1619642375e
Sergey <dyuzhovsergey@gmail.com>
2025-05-27 21:14:04 +0300

    Add functions in package spentenergy

--- internal/spentenergy/spentenergy.go
+++ internal/spentenergy/spentenergy.go
@@ -1,6 +1,7 @@
 package spentenergy
 
 import (
+	"errors"
 	"time"
 )
 
@@ -12,18 +13,57 @@ const (
 	walkingCaloriesCoefficient = 0.5  // коэффициент для расчета калорий при ходьбе.
 )
 
-func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
+func Distance(steps int, height float64) float64 {
 	// TODO: реализовать функцию
-}
-
-func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	// TODO: реализовать функцию
+	lenghtSteps := height * stepLengthCoefficient
+	return (float64(steps) * lenghtSteps) / mInKm
 }
 
 func MeanSpeed(steps int, height float64, duration time.Duration) float64 {
 	// TODO: реализовать функцию
+	if duration <= 0 || steps <= 0 {
+		return 0
+	}
+
+	distance := Distance(steps, height)
+	durationInHour := duration.Hours()
+	return distance / durationInHour
 }
 
-func Distance(steps int, height float64) float64 {
+func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
 	// TODO: реализовать функцию
+	switch {
+	case steps <= 0:
+		return 0, errors.New("invalid input: steps must be > 0")
+	case weight <= 0:
+		return 0, errors.New("invalid input: weight must be > 0")
+	case height <= 0:
+		return 0, errors.New("invalid input: height must be > 0")
+	case duration <= 0:
+		return 0, errors.New("invalid input: duration must be > 0")
+	}
+
+	meanSpeed := MeanSpeed(steps, height, duration)
+	durationInMinutes := duration.Minutes()
+	calories := (weight * meanSpeed * durationInMinutes) / minInH
+	return calories, nil
+}
+
+func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
+	// TODO: реализовать функцию
+	switch {
+	case steps <= 0:
+		return 0, errors.New("invalid input: steps must be > 0")
+	case weight <= 0:
+		return 0, errors.New("invalid input: weight must be > 0")
+	case height <= 0:
+		return 0, errors.New("invalid input: height must be > 0")
+	case duration <= 0:
+		return 0, errors.New("invalid input: duration must be > 0")
+	}
+
+	meanSpeed := MeanSpeed(steps, height, duration)
+	durationInMinutes := duration.Minutes()
+	calories := (weight * meanSpeed * durationInMinutes) / minInH
+	return calories * walkingCaloriesCoefficient, nil
 }
